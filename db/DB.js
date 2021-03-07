598097e05a4cf980b13298d470fa47c677ac32f9
James Fisher <jfisher396@hotmail.com>
2021-03-06 17:26:48 -0800

    delete function refactored

--- db/DB.js
+++ db/DB.js
@@ -7,26 +7,36 @@ const readFileAsync = util.promisify(fs.readFile);
 const writeFileAsync = util.promisify(fs.writeFile);
 
 class DB {
-    async readNotes() {
-        try {
-            const notesRaw = await readFileAsync(noteData, "UTF8");
-            return notesRaw ? JSON.parse(notesRaw) : [];
-        } catch (error) {
-            console.log("Something went wrong READING the notes ", error)
-        }
+  async readNotes() {
+    try {
+      const notesRaw = await readFileAsync(noteData, "UTF8");
+      return notesRaw ? JSON.parse(notesRaw) : [];
+    } catch (error) {
+      console.log("Something went wrong READING the notes ", error);
     }
+  }
 
-    async addNote(data) {
-        try {
-            await writeFileAsync (noteData, JSON.stringify(data, null, "\t")).then(
-                () => {
-                    console.log("New note added.")
-                }
-            )
-        } catch (error) {
-            throw error;
+  async addNote(data) {
+    try {
+      await writeFileAsync(noteData, JSON.stringify(data, null, "\t")).then(() => {
+          console.log("New note added.");
         }
+      );
+    } catch (error) {
+      throw error;
     }
+  }
+
+  async deleteNote(data) {
+      try {
+          await writeFileAsync(noteData, JSON.stringify(data, null, "\t")).then(() => {
+              console.log("Note deleted.")
+          }
+        );
+      } catch (error) {
+          
+      }
+  }
 }
 
 module.exports = new DB();
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,49 +1,42 @@
+// dependencies
 const express = require("express");
 const router = express.Router();
-
-const uuid = require('uuid');
-
+// creates a random id
+const uuid = require("uuid");
+// brings in the DB class object
 const DB = require("../db/DB");
 
+// route to get notes
+router.get("/api/notes", async function (req, res) {
+  const notes = await DB.readNotes();
+  return res.json(notes);
+});
 
+// route to add a new note and add it to the json file
+router.post("/api/notes", async function (req, res) {
+  const currentNotes = await DB.readNotes();
+  let newNote = {
+    id: uuid(),
+    title: req.body.title,
+    text: req.body.text,
+  };
 
-    // route to get notes
-  router.get("/api/notes", async function (req, res) {
-    const notes = await DB.readNotes();
-    return res.json(notes);
-  });
+  await DB.addNote([...currentNotes, newNote]);
 
-    // route to add a new note and add it to the json file
-  router.post("/api/notes", async function (req, res) {
-    const currentNotes = await DB.readNotes();
-    let newNote = {
-        id: uuid(),
-        title: req.body.title,
-        text: req.body.text
-    };
+  return res.send(newNote);
+});
 
-    await DB.addNote([...currentNotes, newNote])
-
-    return res.send(newNote);
-  });
-
-
-  // // route to delete notes
-  // router.delete("/api/notes/:id", function(req,res) {
-  //   // separates out the note to delete based on id
-  //   const noteToDelete = req.params.id;
-  //   // sort through notes file and create a new array minus the note in question
-  //   const newNoteData = noteData.filter(note => note.id !== noteToDelete);
-
-  //   writeASync(__dirname + "/../db/db.json", JSON.stringify(newNoteData, null, "\t")).then(function() {
-  //       console.log("note removed");
-  //   }).catch(function(err) {
-  //       throw err
-  //   }).then(function() {
-  //     window.location.href = "/notes";
-  //   });
-  
-  //   return res.send(noteData)
-  // });
+// // route to delete notes
+router.delete("/api/notes/:id", async function (req, res) {
+  // separates out the note to delete based on id
+  const noteToDelete = req.params.id;
+  // notes already in json file
+  const currentNotes = await DB.readNotes();
+  // sort through notes file and create a new array minus the note in question
+  const newNoteData = currentNotes.filter((note) => note.id !== noteToDelete);
+  // sends the new array back the DB class 
+  await DB.deleteNote(newNoteData);
+  return res.send(newNoteData);
+});
 
 module.exports = router;
